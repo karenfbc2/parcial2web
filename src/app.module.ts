473ca6d9abb5e78f3dd1b8fecf7a88765f56394f
karenfbc <k.fuentesb@uniandes.edu.co>
2025-11-21 08:04:37 -0500

    creacion modulos + orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PonenteModule } from './ponente/ponente.module';
+import { AuditorioModule } from './auditorio/auditorio.module';
+import { AsistenteModule } from './asistente/asistente.module';
+import { EventoModule } from './evento/evento.module';
 
 @Module({
-  imports: [],
+  imports: [PonenteModule, AuditorioModule, AsistenteModule, EventoModule],
   controllers: [AppController],
   providers: [AppService],
 })
